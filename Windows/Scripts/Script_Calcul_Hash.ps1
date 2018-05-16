64b4eb5a19cae5937992ad65914f8796a5fa2a52
Jérémy GAUTIER <gautierjeremy@gmail.com>
2018-05-16 13:19:02 +0200

    modif script hash

--- Windows/Scripts/Script_Calcul_Hash.ps1
+++ Windows/Scripts/Script_Calcul_Hash.ps1
@@ -1,27 +1,32 @@
 param(
 	[string] $path = $(throw "A path is required. Usage: .\Script_Calcul_Hash.ps1 -path 'my_path' -algorithm 'sha256' -day 2 "),
 	[string] $algorithm = 'sha256',
-	[double] $day = 0
+	[double] $day = 0,
+	[string] $reptmp = "C:\"
 )
 
 $day = $day * -1
 $files = gci $path -Recurse | Where-Object { !$_.PSIsContainer } | Where{$_.LastWriteTime -gt (Get-Date).AddDays($day)}
 
 $powershellversion = $PSVersionTable.PSVersion.Major
+$powershellversion
 
 if($powershellversion -ge 4){
 	foreach ($file in $files) {
 		Get-ChildItem $file.FullName -File | Select LastWriteTime,Name,FullName,@{N='FileHash';E={(Get-FileHash $_.PSPath -algorithm SHA256).Hash}},@{N='Algorithm';E={echo $algorithm}} | ConvertTo-JSON -Compress
+		$file
 	}
 }else{
 	$rand = Get-Random
-	$rand_file = "C:\hash_files_"+$algorithm+"_"+$rand+".json"
-	$tmp_file = "C:\tmp.json"
+	$rand_file = $reptmp+"hash_files_"+$algorithm+"_"+$rand+".json"
+	$tmp_file = $reptmp+"tmp.json"
 	foreach ($file in $files) {
-		$hash = [System.Security.Cryptography.HashAlgorithm]::create($algorithm)
-		$Name = $file.Name.ToString().Replace('"','\"').Replace('\','\\').Replace("`n",'').Replace("`r",'').Replace("`t",'')
-		$FullName = $file.FullName.ToString().Replace('"','\"').Replace('\','\\').Replace("`n",'').Replace("`r",'').Replace("`t",'')
-		'{"LastWriteTime":"'+$file.LastWriteTime+'","Name":"'+$Name+'","FullName":"'+$FullName+'","FileHash":"'+[System.BitConverter]::ToString( $hash.ComputeHash([System.IO.File]::ReadAllBytes($file.FullName))).replace('-',"")+'","Algorithm":"'+$algorithm+'"}' | Out-File -Append -FilePath $tmp_file
+		if($file){
+			$hash = [System.Security.Cryptography.HashAlgorithm]::create($algorithm)
+			$Name = $file.Name.ToString().Replace('"','\"').Replace('\','\\').Replace("`n",'').Replace("`r",'').Replace("`t",'')
+			$FullName = $file.FullName.ToString().Replace('"','\"').Replace('\','\\').Replace("`n",'').Replace("`r",'').Replace("`t",'')
+			'{"LastWriteTime":"'+$file.LastWriteTime+'","Name":"'+$Name+'","FullName":"'+$FullName+'","FileHash":"'+[System.BitConverter]::ToString( $hash.ComputeHash([System.IO.File]::ReadAllBytes($file.FullName))).replace('-',"")+'","Algorithm":"'+$algorithm+'"}' | Out-File -Append -FilePath $tmp_file
+		}
 	}
 	Rename-Item -Path $tmp_file -NewName $rand_file
 }
